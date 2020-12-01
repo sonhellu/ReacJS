703f05b547ea5101dbd91855a9f33fef46af332e
sonhellu <sonhellu186@gmail.com>
2020-12-01 16:22:04 +0700

    Homework

--- session3/scrip.js
+++ session3/scrip.js
@@ -1,13 +1,15 @@
-Heloo = (a, b) => {
-    a = 100, b = 200;
-    console.log(a + b);
-    var listNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    console.log("Chieu dai mang la : " + listNumber.length);
-    console.log("Phan tu co index bang 7 la : " + listNumber[7]);
-    listNumber.splice(5, 1);
-    console.log(listNumber);
-    const map = listNumber.map(x => x * 3);
-    console.log(map);
-}
-Heloo();
 
+var numBers =prompt("Nhập Số ");
+switch (numBers){
+    case '1' :
+        window.open("https://quantrimang.com");
+        break;
+    case '2' :
+        window.open(" https://download.com.vn");
+        break;
+    case '3' :
+        window.open("https://vndoc.com");
+        break;
+    default :
+        window.open("https://meta.vn");
+}
